e4a8b5057ba93a26c60dbd06312c8cde17dd037f
H1D30 <bruno_hyk@hotmail.com>
2017-06-29 11:47:37 -0300

    Create lol_chat_settings.c

---
+++ source/lol_chat_settings.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void enable_firewall() {
+	printf("** RUN AS ADMINISTRATOR **\nEnabling firewall... [FORCED]\n");
+	system("netsh advfirewall set all state on");
+	printf("Done.\n");
+	system("pause");
+}
+
+void add_rule() {
+	system("netsh advfirewall firewall add rule name=\"LoL Chat OFF\" dir=out action=block protocol=TCP remoteport=5223");
+	printf("Done.\n");
+}
+
+void del_rule() {
+	system("netsh advfirewall firewall delete rule name=\"LoL Chat OFF\"");
+	printf("Done.\n");
+}
+
+void enable_rule() {
+	system("netsh advfirewall firewall set rule name=\"LoL Chat OFF\" new enable=yes");
+	printf("Done.\n");
+}
+
+void disable_rule() {
+	system("netsh advfirewall firewall set rule name=\"LoL Chat OFF\" new enable=no");
+	printf("Done.\n");
+}
+
+int get_int(){
+	int x,c;
+	scanf(" %d", &x);
+    while((c = getchar()) != '\n' && c != EOF){}
+	return x;
+}
+
+void menu_show(){
+	system("cls");
+	printf("LoL Chat - Set your status: ** RUN AS ADMINISTRATOR **\n1 - Online\n2 - Offline\n\n8 - Install\n9 - Uninstall\n0 - Exit\n\n>");
+}
+
+void menu_action(int opt){
+	switch (opt) {
+		case 0:
+			printf("Bye!\n");
+			break;
+		case 1:
+			disable_rule();
+			break;
+		case 2:
+			enable_rule();
+			break;
+		case 8:
+			add_rule();
+			break;
+		case 9:
+			del_rule();
+			break;
+		default:
+			printf("None selected.");
+	}
+	system("pause");
+}
+
+int main () {
+	int opt = 1;
+	
+	enable_firewall();
+	
+	while (opt) {
+		menu_show();
+		opt = get_int();
+		menu_action(opt);
+	}
+	
+	return 0;
+}
